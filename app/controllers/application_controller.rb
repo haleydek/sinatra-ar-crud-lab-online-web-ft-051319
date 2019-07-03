6aa25b63f07d31e2ce693a3c13a6f775466d2543
Haley DeKeyser <hcd09@live.com>
2019-07-03 18:46:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,7 @@ class ApplicationController < Sinatra::Base
   
   post '/articles' do
     Article.create(params[:article][:title], params[:article][:content])
+    binding.pry
     erb :index
   end
 end
