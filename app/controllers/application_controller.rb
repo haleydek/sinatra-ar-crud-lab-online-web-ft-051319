deb199e3b714ead432ae3f2f74fa646f6df35223
Haley DeKeyser <hcd09@live.com>
2019-07-03 19:52:45 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,8 @@ class ApplicationController < Sinatra::Base
   end
   
   delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.delete
     redirect to("/articles")
   end
   
