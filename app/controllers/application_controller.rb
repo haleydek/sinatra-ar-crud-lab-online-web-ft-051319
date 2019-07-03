736cdee40e5c2a5768dd762ab7e457d94e31c5d8
Haley DeKeyser <hcd09@live.com>
2019-07-03 19:06:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,8 @@ class ApplicationController < Sinatra::Base
   end
   
   post '/articles' do
-    Article.create(title: params[:title], content: params[:content])
-    redirect to("/articles/#{Article.last.id}")
+    @article = Article.create(title: params[:title], content: params[:content])
+    redirect to("/articles/#{@article.id}")
   end
   
   get '/articles/:id' do
